a5a67779a38f47d5f06614ea9a99b1f5089a39ac
maestro1989 <136688300+maestro1989@users.noreply.github.com>
2024-06-28 11:16:19 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,11 @@ if __name__ == '__main__' :
 def main() : 
   st.write('Contoh dataframe')
   st.dataframe(house)
-  
+  st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+  st.write('Menampilkan Dataframe dengan St AgGrid')
+
+
+
+
 if __name__ == '__main__' : 
   main()
