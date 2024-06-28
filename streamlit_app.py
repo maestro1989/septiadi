0e67ebbff7ade1d0cef39008a20024a7b8f314ca
maestro1989 <136688300+maestro1989@users.noreply.github.com>
2024-06-28 11:23:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,12 @@ def main() :
   st.write('Contoh dataframe')
   st.dataframe(house)
   st.metric(label="Temperature", value="70 °F", delta="-1.2 °F")
-
+  # Menampilkan metrik kedua
+  st.metric(label="Wind Speed", value="15 mph", delta="-2 mph")
+  # Menampilkan metrik ketiga tanpa delta
+  st.metric(label="Humidity", value="85%")
+  # Menampilkan metrik dengan warna delta disesuaikan
+  st.metric(label="Stock Price", value="$300", delta="-5%", delta_color="inverse")
 
 
 
