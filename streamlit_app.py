f58bc7e6bf021eddd07460ffbdc14237d90150d2
maestro1989 <136688300+maestro1989@users.noreply.github.com>
2024-06-28 14:07:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,12 +66,13 @@ def main() :
   #sidebar
   with st.form("Data Diri"):
     st.write("Inside the form")
-  slider_val = st.slider("Form slider")
-  checkbox_val = st.checkbox("Form checkbox")
+    slider_val = st.slider("Form slider")
+    checkbox_val = st.checkbox("Form checkbox")
   # Every form must have a submit button.
   submitted = st.form_submit_button("Submit")
   if submitted:
     st.write("slider", slider_val, "checkbox", checkbox_val)
+  
   st.write("Outside the form")
 
 if __name__ == '__main__' : 
